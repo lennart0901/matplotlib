ce8bb12527edd59206b0e692319f2b843b65446b
Lennart Fricke <lennart@die-frickes.eu>
2014-11-11 16:19:21 +0100

    Coding style: Add missing newline

--- lib/matplotlib/tests/test_collections.py
+++ lib/matplotlib/tests/test_collections.py
@@ -546,6 +546,7 @@ def test_linestyle_single_dashes():
     plt.scatter([0, 1, 2], [0, 1, 2], linestyle=(0., [2., 2.]))
     plt.draw()
 
+
 if __name__ == '__main__':
     import nose
     nose.runmodule(argv=['-s', '--with-doctest'], exit=False)
